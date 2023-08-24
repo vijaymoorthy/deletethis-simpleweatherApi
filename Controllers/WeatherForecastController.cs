5df481e766f001b1a8ba888cc59baf43be36ffc9
Vijay Moorthy <vijay.moorthy@chemistwarehouse.com.au>
2023-08-24 14:21:57 +1000

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace simpleweatherApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30)]
+        public WeatherForecastweek Get()
+        {
+            WeatherForecastweek weatherForecastweek = new WeatherForecastweek();
+
+            weatherForecastweek.Forecasts = 
+            Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return weatherForecastweek;
+        }
+    }
+}
